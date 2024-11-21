32093d3bedb008d66a3568c0d72cbd2cf761a638
TheresePinkLizard <therese.trollbu@gmail.com>
2024-11-21 17:40:10 +0100

    Changed the frontpage

--- src/Home/HomePage.jsx
+++ src/Home/HomePage.jsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, Container } from 'react-bootstrap';
+import PinListPage from '../pins/PinListPage';
 
-// displays homepage
 const HomePage = () => {
+    const [content, setContent] = useState(null);
+
+    const handleButtonClick = (buttonName) => {
+        setContent(buttonName);
+    };
+
     return (
-        <div className="text-center">
-            <h1 className="display-4"> Welcome to the homepage!</h1>
-        </div>
+        <Container style={{ display: 'flex', width: '1000px', height: '600px' }}>
+            <div style={{ flex: '0 0 50%' }}>
+                <h3>Find your place</h3>
+                <div id="map" style={{ height: '500px', width: '100%' }}></div>
+                <Button variant="secondary" onClick={() => handleButtonClick('Add a pin')}>Add a pin</Button>
+            </div>
+            <div style={{ flex: '1' }}>
+                <div style={{ display: 'flex', flexDirection: 'row' }}>
+                    <Button variant="primary" onClick={() => handleButtonClick('Button 1')}>Feed</Button>
+                    <Button variant="secondary" onClick={() => handleButtonClick('Button 2')}>My Pins</Button>
+                </div>
+                <div id="feedwindow" style={{ display: content === 'Button 1' ? 'block' : 'none', overflow: 'auto', width: '500px', height: '550px'}}>
+                    <PinListPage />
+                </div>
+                <div id="favwindow" style={{ display: content === 'Button 2' ? 'block' : 'none', overflow: 'auto', width: '500px', height: '550px' }}>
+                    {'You clicked Button 2'}
+                </div>
+            </div>
+        </Container>
     );
 };
-export default HomePage; 
+
+export default HomePage;

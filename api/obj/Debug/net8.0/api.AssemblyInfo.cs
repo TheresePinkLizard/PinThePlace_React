a6978e9d3b0e834b204f86bc01f754f29b7082b3
SolveigJorgensen <113687142+SolveigJorgensen@users.noreply.github.com>
2024-11-26 14:43:49 +0100

    Automatisk endringer

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7fb0e0edecc21357c494de1ce09f90477e59850")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ba3d58976ca48e3f9baecaa1e02eb4891ec7c95")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

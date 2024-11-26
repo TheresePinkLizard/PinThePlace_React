40b8c7ba3d88157afe340aa146b493fe9c11d455
TheresePinkLizard <therese.trollbu@gmail.com>
2024-11-26 13:39:28 +0100

    removed weather and updated program cs, launchJson ad tasksJson

--- api/Program.cs
+++ api/Program.cs
@@ -42,30 +42,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-
-
-app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
-
 app.UseStaticFiles();
 app.UseRouting();
 app.UseCors("CorsPolicy");
@@ -74,7 +50,3 @@ app.MapControllerRoute(name: "api", pattern: "{controller}/{action=Index}/{id?}"
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}

91b6aa2945352a12a74ce06bb8f71ed1effe574c
TheresePinkLizard <therese.trollbu@gmail.com>
2024-11-27 09:28:21 +0100

    Added userlistpage

--- PinThePlace/src/pins/UserListPage.jsx
+++ PinThePlace/src/pins/UserListPage.jsx
@@ -1,47 +1,60 @@
-import React from 'react';
-import { Card, Container, Row, Col } from 'react-bootstrap';
+import React, {useState, useEffect} from 'react';
+import { Card, Container, Row, Col, Table, Button } from 'react-bootstrap';
+
+const API_URL = 'http://localhost:5056'
 
 const UserListPage = () => {
-    // Mock data
-    const userPins = [
-        {
-            PinId: 2,
-            Name: "OsloMet",
-            Rating: 5,
-            Comment: "Bra skole. Anbefales!",
-            Latitude: 59.921365321156706, 
-            Longitude: 10.733315263484577,
-            UserName: "user2",
-            UserId: 2,
-            ImageUrl: "/images/lynx.jpg"
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const fetchUsers = async () => {
+        setLoading(true); // Set loading to true when starting the fetch
+        setError(null);   // Clear any previous errors
+        try {
+            const response = await fetch(`${API_URL}/api/userapi/userlist`); 
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            const data = await response.json();
+            setUsers(data);
+            console.log(data);
+        } catch (error) {
+            console.error(`There was a problem with the fetch operation: ${error.message}`);
+            setError('Failed to fetch users.');
+        } finally {
+            setLoading(false); // Set loading to false once the fetch is complete
         }
-    ];
+    };  
+    useEffect(() => {
+        fetchUsers();
+    }, []);
 
     return (
         <Container>
-        <Row>
-            {userPins.map(pin => (
-                <Col xs={12} key={pin.PinId}>
-                    <Card style={{ marginBottom: '20px' }}>
-                        <Row className="no-gutters">
-                            <Col md={6}>
-                                <Card.Body>
-                                    <Card.Title>{pin.Name}</Card.Title>
-                                    <Card.Text>Rating: {pin.Rating}</Card.Text>
-                                    <Card.Text>Comment: {pin.Comment}</Card.Text>
-                                </Card.Body>
-                            </Col>
-                            <Col md={6}>
-                                <div className="image-container">
-                                    <Card.Img variant="top" src={pin.ImageUrl} alt={pin.Name} />
-                                </div>
-                            </Col>
-                        </Row>
-                    </Card>
-                </Col>
-            ))}
-        </Row>
-    </Container>
+            <Row>
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                            <th>UserId</th>
+                            <th>Username</th>
+                            <th>Email</th>
+                            <th>Pins</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {users.map(user => (
+                            <tr key={user.UserId}>
+                                <td>{user.UserId}</td>
+                                <td>{user.UserName}</td>
+                                <td>{user.Email}</td>
+                                <td>{user.Pins && user.Pins.map(pin => `Pin${pin.PinId}`).join(', ')}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </Table>
+            </Row>
+        </Container>
     );
 };
 
--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59e675df6b786c30c583687eb9507caad4929f22")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8566ae539fa364fb42687ea35f1a0b1f2c6e7640")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

fb45616a6ead91e3bb96d6825f7138a156e57e17
SolveigJorgensen <113687142+SolveigJorgensen@users.noreply.github.com>
2024-11-29 10:15:09 +0100

    MyPins og brukere fungerer

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca25f7f3e1786b2a04957f17ba0e19a74ddd3914")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d5de5b84898e1b966b45d684a7eba731e9a79a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

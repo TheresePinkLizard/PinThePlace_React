8c2b63cd50cc44829c372ef19312164abc70b1f7
97illimo <114436711+97illimo@users.noreply.github.com>
2024-12-01 19:21:01 +0100

    fjerna norske kommentarer DAL

--- api/DAL/PinDbContext.cs
+++ api/DAL/PinDbContext.cs
@@ -1,14 +1,14 @@
-// Kode for database i forhold til shop items. Enkelte nettsider kan ha flere slike database klasser
 
-using Microsoft.EntityFrameworkCore; // importerer Entity framework core funksjoner som er nødvendig for database operasjoner
+
+using Microsoft.EntityFrameworkCore; 
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
-using PinThePlace.Models; // namespace: er en container som holder logisk gruppering av relaterte klasser, interfaces, structs, enums og delegates. Hjelper å organisere kode, unngå navnkonflikt og forbedre koden sin vedlikeholdbarhet
+using PinThePlace.Models; 
 
 namespace PinThePlace.DAL;
-public class PinDbContext : IdentityDbContext<User> // definerer at classen ItemDbContext arver fra DbContext. DbContext representerer en session med databasen. session(midlertidig interaktiv informasjons interchange) det er brukt til å query og lagre data til databasen
+public class PinDbContext : IdentityDbContext<User> 
 {
-    public PinDbContext(DbContextOptions<PinDbContext> options) : base (options) // konstruktør. konfigurerer database connection string
+    public PinDbContext(DbContextOptions<PinDbContext> options) : base (options) 
     {
     
     }                             
